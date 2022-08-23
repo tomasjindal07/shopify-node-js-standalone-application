2c0f3f0a479f19c82548177f981f5593f33cfeba
Tomas Jindal <tomasjindal07@gmail.com>
2022-08-23 13:19:38 +0530

    Update config.js
    Update shopify_app_url URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.config = {
     base_url : "https://e170-103-217-122-25.ngrok.io",
-    shopify_app_url  : "https://apps.shopify.com/sms-notifications-1",
+    shopify_app_url  : "https://apps.shopify.com",
     shopify  : {
         api_key     : "128ca3d93d7170121a4ef29f8e3a6a21",
         api_secret  : "66643553eba3fbb76f99eb078004b077",
